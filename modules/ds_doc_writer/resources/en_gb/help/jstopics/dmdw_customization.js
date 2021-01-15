28099d7768541b0099e64fbaeea44bc672b82f0a
Andre van den Boogert <andre.van.den.boogert@realworld-systems.com>
2021-01-15 16:22:49 +0100

    Added schema xsd on github/boogert-lab and add this to the gis and electricity examples in the camdb_ module

---
+++ modules/ds_doc_writer/resources/en_gb/help/jstopics/dmdw_customization.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Customization",
+hmDescription:"1. Specify a product and module for the Datamodel Version.",
+hmPrevLink:"dmdw_getting-started.html",
+hmNextLink:"dmdw_datamodel_version.html",
+hmParentLink:"index.html",
+hmBreadCrumbs:"",
+hmTitlePath:"Customization",
+hmHeader:"",
+hmBody:"<p class=\"p_Normal\">1. Specify a <span style=\"font-weight: bold;\">product<\/span> and <span style=\"font-weight: bold;\">module<\/span> for the <a href=\"dmdw_datamodel_version.html\" class=\"topiclink\">Datamodel Version<\/a>.<\/p>\n\r<p class=\"p_Normal\">2. <\/p>\n\r"
+})
